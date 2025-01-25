ee5bf7897a0ec547179626f03db30647704e89d6
Eryk Ksciuczyk <48735008+erykksc@users.noreply.github.com>
2025-01-25 21:47:32 +0100

    feat: add randomized pickers of tea ingredients

--- globals.gd
+++ globals.gd
@@ -6,11 +6,21 @@ const TeaBase = {
     ORANGE = "base_orange"
 }
 
+func random_tea_base()->String:
+	var keys := TeaBase.keys()
+	var random_key:String = keys[randi() % keys.size()]
+	return TeaBase[random_key]
+
 const TeaWorm = {
     YELLOW ="worm_yellow",
     RED = "worm_red",
     BLUE = "worm_blue",
 }
+
+func random_tea_worm()->String:
+	var keys := TeaWorm.keys()
+	var random_key:String = keys[randi() % keys.size()]
+	return TeaWorm[random_key]
 
 const ICE = "ice"
 const LID = "lid"
