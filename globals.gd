b47563eb96d68bc3b46197b2d40eb506a4374433
Eryk Ksciuczyk <48735008+erykksc@users.noreply.github.com>
2025-01-25 21:40:09 +0100

    feat: add kubek.gd and globals

---
+++ globals.gd
@@ -0,0 +1,16 @@
+class_name Globals
+
+const TeaBase = {
+    WHITE = "base_white",
+    GREEN = "base_green",
+    ORANGE = "base_orange"
+}
+
+const TeaWorm = {
+    YELLOW ="worm_yellow",
+    RED = "worm_red",
+    BLUE = "worm_blue",
+}
+
+const ICE = "ice"
+const LID = "lid"
