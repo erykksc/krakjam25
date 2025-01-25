6adf445ef1515ef847d22f7ee12ec437f7fcb2a0
Eryk Ksciuczyk <48735008+erykksc@users.noreply.github.com>
2025-01-25 20:21:59 +0100

    feat: add cloud spawning

--- clients/client.gd
+++ clients/client.gd
@@ -4,10 +4,21 @@ extends Node3D
 @export var order_wait_time: float = 3.0
 @export var points_for_order: int = 100
 
+# added on _ready a node under which all clouds are spawned
+var clouds: Node3D
+
+const first_cloud_y_offset:float = 1
+const cloud_margin:float = 0.8
+
+const CLOUD_SCENE := preload("res://cloud/cloud.tscn")
+
 # This variable should be set by the script spawning the client
 var on_order_timeout : Callable = func()->void: pass
 
 func _ready()->void:
+	clouds = Node3D.new()
+	add_child(clouds)
+
 	# start order timer
 	var orderTimer:= Timer.new()
 	orderTimer.one_shot= true
@@ -18,5 +29,13 @@ func _ready()->void:
 	orderTimer.wait_time = order_wait_time
 	add_child(orderTimer)
 	orderTimer.start()
-	
-	
+
+	var new_clouds_count := randi_range(1, 4)
+	for _i in new_clouds_count:
+		addCloud()
+
+func addCloud()->void:
+	var cloudInstance := CLOUD_SCENE.instantiate()
+	clouds.add_child(cloudInstance)
+	var cloudOff:float = first_cloud_y_offset+ cloud_margin*(clouds.get_children().size()-1)
+	cloudInstance.position += Vector3(0,cloudOff,0)
---
+++ cloud/cloud.gd
@@ -0,0 +1,2 @@
+class_name Cloud
+extends Node3D
