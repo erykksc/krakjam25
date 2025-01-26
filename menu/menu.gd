a182194e8ec67f90a702daccc5e7efc796b70337
julia01111 <63444269+PanCake47@users.noreply.github.com>
2025-01-26 13:41:43 +0100

    ft: menu music delete, instrukcja add

--- menu/menu.gd
+++ menu/menu.gd
@@ -1,12 +1,4 @@
 extends Control
 
-@onready var cute_recruit_246084: AudioStreamPlayer = $"Cute-recruit-246084"
-
-func  _ready() -> void:
-	cute_recruit_246084.play()
-
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://game/game.tscn")
-	
-func _process(delta: float) -> void:
-	pass
+	get_tree().change_scene_to_file("res://menu/instrukcja.tscn")
