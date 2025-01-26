ef50cf541c7fdec7617c5731920c486d99abadd7
Eryk Ksciuczyk <48735008+erykksc@users.noreply.github.com>
2025-01-26 05:57:07 +0100

    feat(models): add hamster model

--- room/room.gd
+++ room/room.gd
@@ -5,6 +5,8 @@ extends Node3D
 func _ready() -> void:
 	for body in room.find_children("", "MeshInstance3D", true):
 		var inner: StaticBody3D = body.get_node("StaticBody3D")
+		if not inner:
+			push_warning("No StaticBody3D found for " + body.name + " can't add to group 'interactable'")
 		
 		body.material_overlay = preload("res://shaders/outline_black.tres")
 		
