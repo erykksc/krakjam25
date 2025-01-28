9426fc40e6c1da8f944dfc4ec9a7c49b6d62a2fe
Eryk Ksciuczyk <48735008+erykksc@users.noreply.github.com>
2025-01-28 02:20:19 +0100

    fix: continue button in high-score view

--- highscore/highscore_view.gd
+++ highscore/highscore_view.gd
@@ -17,6 +17,6 @@ func _ready() -> void:
 
 	highscoresLabel.text = "\n".join(unique_scores)
 
-func _on_continue_button_press() -> void:
-	print("continue button press")
+func _on_continue_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://menu/menu.tscn")
+
